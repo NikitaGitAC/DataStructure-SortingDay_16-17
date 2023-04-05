6b4be9adbe0cd5aeadfa35e0b3cbd5d613624f9d
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-05 12:36:58 +0530

    display bubble sort value

--- Program.cs
+++ Program.cs
@@ -5,15 +5,7 @@
         public static void Main(string[]args)
         {
             Console.WriteLine("Data Structures and Algorithms Programs");
-            binarytreecode binaryTree = new binarytreecode();
-
-            binaryTree.Add(50);
-            binaryTree.Add(20);
-            binaryTree.Add(30);
-            binaryTree.Add(15);
-            binaryTree.Add(60);
-            binaryTree.Add(80);
-            binaryTree.TravarsePreorder(binaryTree.Root);
+            Bubblesort.Displaybubblesort();
         }
     }
 }
