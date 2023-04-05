b724d897a64f06da117d93204b128e0868dfa926
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-05 12:47:58 +0530

    display insertion sort value

---
+++ insertionsort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binarytree
+{
+    public class insertionsort
+    {
+        public void DisplayInsertionsort()
+        {
+            int[] arr = new int[5] { 23, 9, 10, 14, 6 };
+            int n = 5, i, j, val, flag;
+            Console.WriteLine("insertion sort");
+            Console.WriteLine("intial array");
+            for (i=0;i<n;i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+              for (i=1;i<n;i++)
+              {
+                val = arr[i];
+                flag = 0;
+                for(j=i-1;j>=0 && flag!=1;)
+                {
+                    if (val < arr[j])
+                    {
+                        arr[j + 1] = arr[j];
+                        j--;
+                        arr[j + 1] = val;
+                    }
+                    else flag = 1;
+                }
+              }
+            Console.WriteLine("sorted array ");
+            for (i=0; i < n; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
