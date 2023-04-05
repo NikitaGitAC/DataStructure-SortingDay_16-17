90b996e3e3be7428b247b154114416200b47dbfe
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-05 12:30:17 +0530

    assign node

---
+++ node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binarytree
+{
+    public class node
+    {
+        internal node leftnode;
+
+        public node LeftNode { get; set; }
+        public node RightNode { get; set; }
+
+
+        public int Data { get; set; }
+    }
+}
