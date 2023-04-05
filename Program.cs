6deea27164730c15064b5d7f21436d1a111d803d
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-05 12:26:39 +0530

    initalize value in a node

--- Program.cs
+++ Program.cs
@@ -5,7 +5,15 @@
         public static void Main(string[]args)
         {
             Console.WriteLine("Data Structures and Algorithms Programs");
-            
+            binarytreecode binaryTree = new binarytreecode();
+
+            binaryTree.Add(50);
+            binaryTree.Add(20);
+            binaryTree.Add(30);
+            binaryTree.Add(15);
+            binaryTree.Add(60);
+            binaryTree.Add(80);
+            binaryTree.TravarsePreorder(binaryTree.Root);
         }
     }
 }
